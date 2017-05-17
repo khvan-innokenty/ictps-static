fa86833d541929c62a676974cc93406a6b0bd4b5
khvan-innokenty <khvan.innokenty@gmail.com>
2017-05-17 14:06:23 +0300

    Добавили блок о курсе для руководителей. Убрали стоимость из формы регистрации

--- dist/app.js
+++ dist/app.js
@@ -25,6 +25,33 @@
         $hamburger.removeClass('active');
         $navigation.removeClass('active');
     });
+
+    // Select all links with hashes
+    $('.js-smooth')
+        .click(function(event) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 300, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        });
 })( jQuery, document, window );
 // jQuery RoyalSlider plugin. Custom build. Copyright 2011-2015 Dmitry Semenov http://dimsemenov.com 
 // http://dimsemenov.com/private/home.php?build=auto-height 
